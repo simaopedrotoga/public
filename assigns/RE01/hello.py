5eac60401f6756c47088926b57e179fb894828b7
Correia Lopes <jlopes@fe.up.pt>
2019-09-09 18:52:30 +0100

    added hello world

---
+++ assigns/RE01/hello.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# FPRO, assign RE01
+#
+# Created by: jlopes
+#
+# Name          : <your name>
+# Collaborators : <your collaborators>
+# Time spent    : <total time>
+
+# my first variable
+greeting = "Hello"
+
+#
+# COMMENTING LINES #######
+#
+# to comment MANY lines at a time, highlight all of them then CTRL+1
+# do CTRL+1 again to uncomment them
+# try it on the next two commented lines below!
+
+# whoami = "FIXME"
+# greeting = greeting + " " + whoami
+
+# output the greeting
+print("\n" + greeting + "!")
