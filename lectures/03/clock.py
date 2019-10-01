deac7842164a313757b855814fb9595a8fae79d3
Correia Lopes <jlopes@fe.up.pt>
2019-10-01 17:08:39 +0100

    Small changes in NB03

---
+++ lectures/03/clock.py
@@ -0,0 +1,25 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("green")
+
+cyril = turtle.Turtle()
+cyril.pensize(5)       # just to notice better
+cyril.shape("turtle")
+cyril.color("blue")
+
+cyril.stamp()
+
+for _ in range(12):
+    cyril.up()
+    cyril.forward(80)  # line offset relative to center
+    cyril.down()
+    cyril.forward(10)  # line length
+    cyril.up()
+    cyril.forward(15)  # stamp offset relative to center
+    cyril.stamp()
+    cyril.backward(105)  # almost all the way back (5 units offset to center clock on turtle center, otherwise will center on upper left corner)
+    cyril.left(30)
+
+wn.exitonclick()
+turtle.bye()
--- lectures/03/herd.py
+++ lectures/03/herd.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 16:37:53 2019
+
+@author: jlopes
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Sun Sep 30 16:31:08 2018
-
 @author: jlopes
-
 Code adapted from the book:
-
 Peter Wentworth, Jeffrey Elkner, Allen B. Downey, and Chris Meyers, 
 How to Think Like a Computer Scientist — Learning with Python 3, 2012
 """
@@ -21,30 +26,30 @@ window.bgcolor("lightgreen")
 window.title("Tess & Alex")
 
 tess = turtle.Turtle()     # Create tess and set some attributes
-tess.color("hotpink")
+tess.color("Pink")
 tess.pensize(5)
 
-alex = turtle.Turtle()     # Create alex
+alex = turtle.Turtle()     # Create alex and set some attributes
+alex.color("Navy")
 
-tess.forward(80)           # Make tess draw equilateral triangle
+tess.forward(160)           # Make tess draw equilateral triangle
 tess.left(120)
-tess.forward(80)
+tess.forward(160)
 tess.left(120)
-tess.forward(80)
+tess.forward(160)
 tess.left(120)             # Complete the triangle
 
 tess.right(180)            # Turn tess around
-tess.forward(80)           # Move her away from the origin
+tess.forward(160)           # Move her away from the origin
 
-alex.forward(50)           # Make alex draw a square
+alex.forward(100)           # Make alex draw a square
 alex.left(90)
-alex.forward(50)
+alex.forward(100)
 alex.left(90)
-alex.forward(50)
+alex.forward(100)
 alex.left(90)
-alex.forward(50)
-alex.left(90) 
+alex.forward(100)
+alex.left(90)
 
 window.mainloop()
 turtle.bye()
-
