5f58389e19f172829496724bdbac5622a590a774
João Correia Lopes <jlopes60@users.noreply.github.com>
2019-11-27 22:07:01 +0000

    Update quicksort.py

--- lectures/18/quicksort.py
+++ lectures/18/quicksort.py
@@ -15,9 +15,9 @@ def quicksort(lst):
         return lst
     pivot = lst[0]
     pivots = [x for x in lst if x == pivot]
-    small = quicksort([x for x in lst if x < pivot])
-    large = quicksort([x for x in lst if x > pivot])
-    return small + pivots + large
+    smaller = quicksort([x for x in lst if x < pivot])
+    larger = quicksort([x for x in lst if x > pivot])
+    return smaller + pivots + larger
 
 
 l = [5, 8, 2, 9, 1, 4, 8, 9, 0, 3, 2, 7]
