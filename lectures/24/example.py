d35807802b05c2417dd25433507a8f838c531666
Correia Lopes <jlopes@fe.up.pt>
2019-12-18 22:19:22 +0000

    NB24 with slides

--- lectures/24/example.py
+++ lectures/24/example.py
@@ -14,11 +14,11 @@ for number in number_list:
     try:
         number_factorial = math.factorial(number)
     except TypeError:
-        print("Factorial is not supported for given input type.")
+        print("\nFactorial is not supported for given input type.")
     except ValueError:
-        print("Factorial only accepts positive integer values.", number, " is not a positive integer.")
+        print("\nFactorial only accepts positive integer values.", number, " is not a positive integer.")
     else:
-        print("The factorial of", number, "is", number_factorial)
+        print("\nThe factorial of", number, "is", number_factorial)
     finally:
         print("Release any resources in use.")
 
